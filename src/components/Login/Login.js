3c34eb81f6fd82bcf17c0c780d06e3a7b4ddfb39
giatha0 <thao8c89@gmail.com>
2023-06-06 02:39:16 +0700

    form login, layout voi bootstrap

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import './Login.scss';
 
 const Login = (props) => {
     return (
-        <div className="login-container">
-            <h1>Login</h1>
-            <button >hi</button>
+        <div className="login-container mt-3">
+            <div className="container">
+                <div className="row">
+                    <div className="content-left col-7">
+                        <div className='brand'>
+                            Thao Duong Gia
+                        </div>
+                        <div className='detail'>
+                            Thao Duong Gia helps you connect and share with the people in your life.
+                        </div>
+                    </div>
+                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                        <input className='form-control' type="text" placeholder="Email address or phone number" />
+                        <input className='form-control' type="password" placeholder="Password" />
+                        <button className='btn btn-primary'>Log In</button>
+                        <span className='text-center'>Fotgot Your Password </span>
+                        <hr />
+                        <div className='text-center'>
+                            <button className='btn btn-success'>Create New Account</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
