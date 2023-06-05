b0b2394372477d74eae30894783b0c232290b187
giatha0 <thao8c89@gmail.com>
2023-06-06 02:11:36 +0700

    them bootstrap, react router dom

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+
+const Login = (props) => {
+    return (
+        <div className="login-container">
+            <h1>Login</h1>
+            <button >hi</button>
+        </div>
+    );
+}
+
+export default Login;
