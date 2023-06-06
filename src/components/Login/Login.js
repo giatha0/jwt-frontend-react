d342fa6992f668be3cc6ddf8d3ddb1741a42d89e
giatha0 <thao8c89@gmail.com>
2023-06-06 14:35:26 +0700

    form login reponsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,10 @@ import './Login.scss';
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container ">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm-7 d-sm-block">
                         <div className='brand'>
                             Thao Duong Gia
                         </div>
@@ -14,11 +14,18 @@ const Login = (props) => {
                             Thao Duong Gia helps you connect and share with the people in your life.
                         </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3 ">
+                        <div className='brand d-sm-none d-block '>
+                            Thao Duong Gia
+                        </div>
                         <input className='form-control' type="text" placeholder="Email address or phone number" />
                         <input className='form-control' type="password" placeholder="Password" />
                         <button className='btn btn-primary'>Log In</button>
-                        <span className='text-center'>Fotgot Your Password </span>
+                        <span className='text-center'>
+                            <a href="#" className='forgot-password'>
+                                Forgot Password
+                            </a>
+                        </span>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success'>Create New Account</button>
