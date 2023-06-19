e354b0d0937bc4731f72ff574a381a89fa247c49
giatha0 <thao8c89@gmail.com>
2023-06-19 15:40:06 +0700

    goi api create new role

--- src/components/Role/Role.js
+++ src/components/Role/Role.js
@@ -2,27 +2,32 @@ import { useEffect, useState } from 'react';
 import './Role.scss';
 import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
+import { createRole } from '../../services/roleService';
 
 
 const Role = (props) => {
 
 
+    const dataChildDefault = { url: '', description: '', isValidUrl: true };
+
     const [listChilds, setListChilds] = useState({
-        child1: { url: '', description: '' }
+        child1: dataChildDefault
     })
-
-
-
 
     const handleOnchangeInput = (name, value, key) => {
         let _listChilds = _.cloneDeep(listChilds);
         _listChilds[key][name] = value;
+        if (name === 'url') {
+            _listChilds[key].isValidUrl = true;
+        }
+
         setListChilds(_listChilds);
     }
 
     const handleAddNewInput = () => {
         let _listChilds = _.cloneDeep(listChilds);
-        _listChilds[`child${uuidv4()}`] = { url: '', description: '' };
+        _listChilds[`child${uuidv4()}`] = dataChildDefault;
         setListChilds(_listChilds);
     }
 
@@ -31,6 +36,46 @@ const Role = (props) => {
         delete _listChilds[key];
         setListChilds(_listChilds);
     }
+
+    const buildDataToPersist = () => {
+        let _listChilds = _.cloneDeep(listChilds);
+        let result = [];
+        Object.entries(_listChilds).find(([key, value], index) => {
+            result.push({
+                url: value.url,
+                description: value.description
+            })
+        })
+        return result;
+    }
+
+    const handleSave = async () => {
+
+        let inValidObject = Object.entries(listChilds).find(([key, value], index) => {
+            return value && !value.url
+        })
+
+        console.log(inValidObject);
+        if (!inValidObject) {
+            // call api
+            let data = buildDataToPersist();
+            console.log('data1', data);
+            let res = await createRole(data);
+            console.log(res);
+            if (res && +res.EC === 0) {
+                toast.success(res.EM);
+            } else {
+                toast.error(res.EM);
+            }
+        } else {
+            // error
+            toast.error('Please enter url');
+            let _listChilds = _.cloneDeep(listChilds);
+            _listChilds[inValidObject[0]].isValidUrl = false;
+            setListChilds(_listChilds);
+        }
+    }
+
 
     return (
         <>
@@ -43,6 +88,7 @@ const Role = (props) => {
                         <div className='role-parent'>
                             {
                                 Object.entries(listChilds).map(([key, value], index) => {
+                                    // console.log('1', value);
                                     return (
                                         <>
                                             <div className={`row role-child ${key}`}>
@@ -50,7 +96,7 @@ const Role = (props) => {
                                                     <label>URL: </label>
                                                     <input
                                                         type='text'
-                                                        className='form-control'
+                                                        className={value.isValidUrl ? 'form-control' : 'form-control is-invalid'}
                                                         value={value.url}
                                                         onChange={(e) => handleOnchangeInput('url', e.target.value, key)}
                                                     />
@@ -85,7 +131,10 @@ const Role = (props) => {
 
                         </div>
                         <div>
-                            <button className='btn btn-warning mt-3'>Save</button>
+                            <button
+                                className='btn btn-warning mt-3'
+                                onClick={handleSave}
+                            >Save</button>
                         </div>
                     </div>
                 </div>
