cf07c67a2eb939b97b25308b3275ec0e4c672ec4
giatha0 <thao8c89@gmail.com>
2023-06-19 22:55:15 +0700

    call api fetch all role and call api delete role

--- src/services/roleService.js
+++ src/services/roleService.js
@@ -1,9 +1,23 @@
 import axios from '../setup/axios';
 
+const fetchAllRoles = (page, limit) => {
+    if (!page && !limit) return axios.get('/api/v1/role/show')
+
+    return axios.get(`/api/v1/role/show?page=${page}&limit=${limit}`)
+}
+
 const createRole = (roles) => {
     return axios.post('/api/v1/role/create', [...roles])
 }
 
+const deleteRole = (role) => {
+    return axios.delete(`/api/v1/role/delete`, { data: { id: role.id } })
+}
+
+const fetchRoleByGroup = (groupId) => {
+    return axios.get(`/api/v1/role/by-group/${groupId}`)
+}
+
 export {
-    createRole
+    createRole, fetchAllRoles, deleteRole, fetchRoleByGroup
 }
